0ecd973b8a7dc8abc650af6d6700fe7307b18033
cmurphy <cmurphy@micron.com>
2015-11-12 16:01:18 -0600

    Added logging.

--- app/__init__.py
+++ app/__init__.py
@@ -2,30 +2,30 @@ from datetime import timedelta
 from flask import Flask
 from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
+import logging
+from logging.handlers import RotatingFileHandler
 
+
+# setup app
 myapp = Flask(__name__)
-
 myapp.config.from_object('config')
 db = SQLAlchemy(myapp)
 lm = LoginManager()
 lm.init_app(myapp)
 lm.login_view = '_login'
 
+# setup logging
+file_handler = RotatingFileHandler('autobench.log', 'a',
+                                   1 * 1024 * 1024, 10)
+file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: '
+                                            '%(message)s [in %(pathname)s:'
+                                            '%(lineno)d]'))
+myapp.logger.setLevel(logging.INFO)
+file_handler.setLevel(logging.INFO)
+myapp.logger.addHandler(file_handler)
+myapp.logger.info('microblog startup')
 
-if not myapp.debug:
-    import logging
-    from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('autobench.log', 'a',
-                                       1 * 1024 * 1024, 10)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: '
-                                                '%(message)s [in %(pathname)s:'
-                                                '%(lineno)d]'))
-    myapp.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    myapp.logger.addHandler(file_handler)
-    myapp.logger.info('microblog startup')
-
-
+# configure environment
 myapp.jinja_env.trim_blocks = True
 myapp.jinja_env.lstrip_blocks = True
 myapp.jinja_env.keep_trailing_newline = False
